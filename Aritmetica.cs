310da8ed4ea8ac7c244cbd8c5371ae66e9ad60e9
Maria Gomes <146724727+MariaGomes2023@users.noreply.github.com>
2023-10-16 20:52:46 +0100

    adicionar comentarios XML aos metodos somar e subtrair

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -9,13 +9,45 @@ namespace Calculos
     // implementa operaçoes aritmeticas
     internal class Aritmetica
     {
+        /// <summary>
+        /// tipo de conversão de temperatura a executar.
+        /// </summary>
+       public enum ConversaoTemperatura
+        {
+            Nulo=0;
+            CelsiusFahrenheit,
+            FahrenheitCelsius
+        }
+        /// <summary>
+        /// Operação Soma   
+        /// </summary>
+        /// <returns>Retorna a soma de 2 numeros.</returns>
         public int somar( int x, int y)
         {
             return x + y;
         }
+       /// <summary>
+       /// Operação de subtração
+       /// </summary>
+       /// <returns>Retorna o resultado da subtração de 2 numeros.</returns>
         public int subtrair(int x, int y)
         {
             return x - y;
+        }
+        public double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
+        {
+            if(conversao == ConversaoTemperatura.CelsiusFahrenheit)
+            {
+                return (temperatura * 1.8000 + 32);
+            }
+                
+            else if(conversao == ConversaoTemperatura.FahrenheitCelsius)
+            {
+                return ((temperatura - 32) / 1.8000);
+            }              
+
+            return -1;
+            
         }
     }
 }
